efac7a9aa7c5a9dc6a54ef7538e6ccd85d3c6dba
nogi2k2 <ashutoshrai20bcs20@iiitkottayam.ac.in>
2021-05-13 02:34:54 +0530

    second commit

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 import random
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
 client= commands.Bot(command_prefix="^")
+
 
 game = ['rock','paper','scissors']
 reactionwon = ['gottee!','ez bwoi','bruh, I won ;-;']
@@ -10,6 +13,27 @@ reactionlost = ['wtf how!','how did you..','sheer luck mate!']
 @client.event
 async def on_ready():
     print("Bot Ready for use")
+
+# driver = webdriver.Chrome()
+# driver.get('https://www.cleverbot.com')
+# driver.find_element_by_id('noteb').click()
+
+# def get_response(message):
+#     driver.find_element_by_xpath('//*[@id="avatarform"]/input[1]').send_keys(message + Keys.RETURN)
+#     while True:
+#         try:
+#             driver.find_element_by_xpath('//*[@id="snipTextIcon"]')
+#             break
+#         except:
+#             continue
+#     response = driver.find_element_by_xpath('//*[@id="line1"]/span[1]').text
+#     return response
+
+#     async def on_message(self, message):
+#         if message.author != self.user:
+#             reponse = get_response(message.content)
+#             await message.channel.send({reponse})
+
 
 @client.command(aliases=['hi','Hi','Hello'])
 async def hello(ctx):
@@ -23,26 +47,26 @@ async def clear(ctx, amount = 2):
 @client.command(aliases=['k','K','Kick','throw'])
 @commands.has_permissions(kick_members = True)
 async def kick(ctx,member : discord.Member,*,reason="Because you suck!"):
-    await ctx.send(member.discord+"has been banned for :"+"\n"+"||"+reason+"||")
+    await ctx.send(f"{member}has been kicked for :"+"\n"+"||"+reason+"||")
     await member.kick(reason = reason)
 
 @client.command(aliases=['b','B','Ban'])
 @commands.has_permissions(ban_members = True)
 async def ban(ctx,member : discord.Member,*,reason="Because you suck!"):
-    await ctx.send(member.discord+"has been banned for :"+"\n"+"||"+reason+"||")
+    await ctx.send(f"{member}banned for :"+"\n"+"||"+reason+"||")
     await member.ban(reason = reason)
 
 @client.command(aliases = ['ub','UB','Unban'])
 @commands.has_permissions(ban_members = True)
 async def unban(ctx,*,member):
-    ban_users = await ctx.guild.bans()
-    member_name,member_disc=member.split('^')
+    banned_users = await ctx.guild.bans()
+    member_name,member_disc=member.split("#")
 
     for banned_entry in banned_users:
         user = banned_entry.user
         if(user.name,user.discriminator) == (member_name,member_disc):
             await ctx.guild.unban(user)
-            await ctx.send(member_name+"has been unbanned")
+            await ctx.send(f"{user.mention} has been unbanned")
             return
     await ctx.send(member+"not found")
 
@@ -54,9 +78,10 @@ async def mute(ctx,member : discord.Member):
     await ctx.send(member.mention+"has been muted")
     
 
-
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     msg=message.content
     if msg.startswith('^rock'):
         choice = random.choice(game)
@@ -87,7 +112,8 @@ async def on_message(message):
             await message.channel.send(random.choice(reactionlost))
         else:
             await message.channel.send("It\'s a draw bruh!")
+    await client.process_commands(message)
 
 
 
-client.run("ODM3MDIzMTUxMDM2Njk0NjIw.YImgjg.7z5pDYexn7YUOQbtnpVAZXOCfLc")
+client.run("ODM3MDIzMTUxMDM2Njk0NjIw.YImgjg.51dlHAhsoDm1Gt1plbHXaec0qiw")
